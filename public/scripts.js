9862334612a9345a22cd6ea09ea575f170a98d4c
Alfredo Sumaran <a.sumaran@gmail.com>
2021-07-15 15:25:46 -0500

    Click timeouts in both Apple Pay and Payment Request API

--- public/scripts.js
+++ public/scripts.js
@@ -17,33 +17,131 @@ function addToCart() {
 }
 
 function onApplePayButtonClicked() {
+  const t0 = performance.now();
+
   if (!ApplePaySession) {
     return;
   }
 
-  addToCart().then(function(newRequest) {
-    setTimeout(function() {
-      const session = new ApplePaySession(3, newRequest);
-      session.onvalidatemerchant = async event => {
-        // Call your own server to request a new merchant session.
-        const merchantSession = await validateMerchant();
-        console.log('merchantSession –>', merchantSession);
-        session.completeMerchantValidation(merchantSession);
+  const request = {
+    "countryCode": "US",
+    "currencyCode": "USD",
+    "merchantCapabilities": [
+      "supports3DS",
+    ],
+    "supportedNetworks": [
+      "visa"
+    ],
+    "total": {
+      "label": "Demo (Card is not charged)",
+      "amount": "1.99",
+      "type": "final"
+    }
+  };
+
+  setTimeout(function() {
+    const session = new ApplePaySession(3, request);
+
+    session.onvalidatemerchant = async event => {
+      const merchantSession = await validateMerchant();
+      session.completeMerchantValidation(merchantSession);
+    };
+
+    session.onpaymentauthorized = event => {
+      console.log('onpaymentauthorized –>', event);
+      const result = {
+        "status": ApplePaySession.STATUS_SUCCESS
       };
-      session.onpaymentauthorized = event => {
-        console.log('success –>', event);
-        // Define ApplePayPaymentAuthorizationResult
-        const result = {
-          "status": ApplePaySession.STATUS_SUCCESS
+      session.completePayment(result);
+    };
+
+    session.begin();
+    const t1 = performance.now();
+    console.log(`1 took ${t1 - t0} milliseconds.`);
+  }, 999);
+}
+
+
+function onApplePayButtonClicked2() {
+    const t0 = performance.now();
+
+    // Consider falling back to Apple Pay JS if Payment Request is not available.
+    if (!PaymentRequest) {
+        return;
+    }
+
+    try {
+        // Define PaymentMethodData
+        const paymentMethodData = [{
+            "supportedMethods": "https://apple.com/apple-pay",
+            "data": {
+                "version": 3,
+                "merchantIdentifier": "merchant.com.example",
+                "merchantCapabilities": [
+                    "supports3DS"
+                ],
+                "supportedNetworks": [
+                    "amex",
+                    "discover",
+                    "masterCard",
+                    "visa"
+                ],
+                "countryCode": "US"
+            }
+        }];
+        // Define PaymentDetails
+        const paymentDetails = {
+            "total": {
+                "label": "My Merchant",
+                "amount": {
+                    "value": "27.50",
+                    "currency": "USD"
+                }
+            }
         };
-        session.completePayment(result);
-      };
+        // Define PaymentOptions
+        const paymentOptions = {
+            "requestPayerName": false,
+            "requestBillingAddress": false,
+            "requestPayerEmail": false,
+            "requestPayerPhone": false,
+            "requestShipping": true,
+            "shippingType": "shipping"
+        };
+        
+        // Create PaymentRequest
+        const request = new PaymentRequest(paymentMethodData, paymentDetails, paymentOptions);
+        console.log('request –>',request);
+        request.onmerchantvalidation = async (event) => {
+            // Call your own server to request a new merchant session.
+            const merchantSessionPromise = await validateMerchant();
+            console.log('merchantSessionPromise –>',merchantSessionPromise);
+            event.complete(merchantSessionPromise);
+        };
+    
+        request.onshippingaddresschange = event => {
+            // Define PaymentDetailsUpdate based on a shipping address change.
+            const paymentDetailsUpdate = {
+                "total": {
+                    "label": "My Merchant",
+                    "amount": {
+                        "value": "27.50",
+                        "currency": "USD"
+                    }
+                }
+            };
+            event.updateWith(paymentDetailsUpdate);
+        };
 
-      session.oncancel = event => {
-        // Payment cancelled by WebKit
-      };
+        setTimeout(async function () {
+          const t1 = performance.now();
+          console.log(`1 took ${t1 - t0} milliseconds.`);
+          const response = await request.show();
+          const status = "success";
+          await response.complete(status);
+        }, 999);
 
-      session.begin();
-    }, 0);
-  });
+    } catch (e) {
+      console.error(e);
+    }
 }
